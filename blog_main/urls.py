af829d04b575658565b91372db395d416b06fca3
Jenish Panchasara <jenishpanchasara83@gmail.com>
2025-12-31 14:10:46 +0530

    Added Dashboard-page

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -23,12 +23,16 @@ from app import views as BlogsView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login, name='login'),
     path('',views.home, name='home'),
     path('category/', include('app.urls')),
     path('blogs/<slug:slug>/', BlogsView.blogs, name='blogs'),
     # Search Endpoint
     path('blogs/search/', BlogsView.search, name='search'),
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    
+    #dashboard
+    path('dashboard/', include('dashboards.urls')),
+    
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
