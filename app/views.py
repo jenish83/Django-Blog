e4d4bd70bda45d5de4f4023fcc4f1dd0c4dcb866
Jenish Panchasara <jenishpanchasara83@gmail.com>
2026-01-02 13:25:39 +0530

    Comment Feature added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from django.http import HttpResponse
-from .models import Blog,Category
+from django.http import HttpResponseRedirect
+from .models import Blog,Category,Comment
 from django.db.models import Q
 
 # Create your views here.
@@ -23,8 +23,23 @@ def posts_by_category(request,category_id):
 
 def blogs(request,slug):
     single_blog = get_object_or_404(Blog, slug=slug, status=1)
+    
+    if request.method == "POST":
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST['comment']
+        comment.save()
+        # this will take you to the page where you came from 
+        return HttpResponseRedirect(request.path_info)
+    
+    
+    comments = Comment.objects.filter(blog=single_blog)
+    comment_count = comments.count()
     context = {
         'single_blog': single_blog,
+        'comments': comments,
+        'comment_count': comment_count,
     }
     return render(request, 'blogs.html',context)
 
